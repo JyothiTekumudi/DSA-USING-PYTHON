4cbfaadb1f5d5ed0140a9cf4af5fbba8172459c9
Veera Siva Naga Jyothi Tekumudi <117423475+JyothiTekumudi@users.noreply.github.com>
2023-07-17 21:33:27 -0700

    Add files via upload

---
+++ linear search.py
@@ -0,0 +1,10 @@
+#linear search
+item=int(input())
+l=[10,23,20,45,89]
+for i in l:
+    if i==item:
+        print("element is found")
+        break
+else:
+        print("element is not found")
+        
